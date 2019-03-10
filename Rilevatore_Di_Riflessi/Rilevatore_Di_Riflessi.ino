295665bbab7469c0be57c56f6a35c118cfe60336
Marco Alessio <alessiomarco02@gmail.com>
2019-03-10 11:24:59 +0100

    Utilizzo func millis()

--- Rilevatore_Di_Riflessi/Rilevatore_Di_Riflessi.ino
+++ Rilevatore_Di_Riflessi/Rilevatore_Di_Riflessi.ino
@@ -1,17 +1,35 @@
 #include <LiquidCrystal_I2C.h>
+#define led 8
+#define pulsante1 2
+#define pulsante2 4
+#define pulsante3 6
+#define verde 10
+#define rosso 12
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup(){
+  pinMode(led, OUTPUT);
+  pinMode(verde, OUTPUT);
+  pinMode(rosso, OUTPUT);
+  pinMode(pulsante1, INPUT);
+  pinMode(pulsante2, INPUT);
+  pinMode(pulsante3, INPUT);
   lcd.init();
   lcd.backlight();
 }
 
 void loop(){
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Progetto Arduino");
-  lcd.setCursor(0, 1);
-  lcd.print("Rileva riflessi");
-  delay(1000);
+  while (digitalRead(pulsante1) == HIGH) {
+    lcd.setCursor(0, 0);
+    lcd.print(millis());
+    lcd.print("ms");
+    delay(1000);
+  }
+  while (digitalRead(pulsante2) == HIGH){
+    lcd.setCursor(0, 1);
+    lcd.print(millis());
+    lcd.print("ms");
+    delay(1000);
+  }
 }
