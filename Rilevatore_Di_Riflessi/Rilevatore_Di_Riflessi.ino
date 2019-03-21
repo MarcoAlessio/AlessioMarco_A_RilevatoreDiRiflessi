a26e0db9d08daae12def5d9ce392537b5df91db8
Marco Alessio <alessiomarco02@gmail.com>
2019-03-21 08:56:03 +0100

    Programma quasi finito
    Ridotto numero delle righe di codice, inserito un unico ciclo per led e buzzer

--- Rilevatore_Di_Riflessi/Rilevatore_Di_Riflessi.ino
+++ Rilevatore_Di_Riflessi/Rilevatore_Di_Riflessi.ino
@@ -1,17 +1,22 @@
 #include <LiquidCrystal_I2C.h>
 #define led 8
-#define pulsante1 2
-#define pulsante2 4
+#define pulsante1 4
+#define pulsante2 5
 #define pulsante3 6
-#define verde 10
-#define rosso 12
+#define buzzer 7
+#define VERDE 10
+#define ROSSO 11
+#define BLU 12
+int counter = 0;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup(){
   pinMode(led, OUTPUT);
-  pinMode(verde, OUTPUT);
-  pinMode(rosso, OUTPUT);
+  pinMode(VERDE, OUTPUT);
+  pinMode(ROSSO, OUTPUT);
+  pinMode(BLU, OUTPUT);
+  pinMode(buzzer, OUTPUT);
   pinMode(pulsante1, INPUT);
   pinMode(pulsante2, INPUT);
   pinMode(pulsante3, INPUT);
@@ -19,17 +24,28 @@ void setup(){
   lcd.backlight();
 }
 
-void loop(){
-  while (digitalRead(pulsante1) == HIGH) {
-    lcd.setCursor(0, 0);
-    lcd.print(millis());
-    lcd.print("ms");
-    delay(1000);
+void ciclo(int oggetto, int pulsante, int riga){
+  delay(random(2000, 15000));
+  digitalWrite(oggetto, HIGH);
+  if(oggetto == buzzer)
+    counter = 0;
+  while(digitalRead(pulsante)== LOW){
+    counter++;
+    delay(1);
   }
-  while (digitalRead(pulsante2) == HIGH){
-    lcd.setCursor(0, 1);
-    lcd.print(millis());
-    lcd.print("ms");
-    delay(1000);
+  digitalWrite(buzzer, LOW);
+  lcd.setCursor(0, riga);
+  lcd.print(counter);
+  lcd.print("ms");
+  if(counter <= 500)
+    digitalWrite(ROSSO, HIGH);
+  else
+    digitalWrite(VERDE, HIGH);
+}
+
+void loop(){
+  if(digitalRead(pulsante1)== HIGH){
+     ciclo(led, pulsante2, 0);
+     ciclo(buzzer, pulsante3, 1);
   }
 }
