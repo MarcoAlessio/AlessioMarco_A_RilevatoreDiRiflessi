f78965173ae8a921c601530ae9af098d388233e4
Marco Alessio <alessiomarco02@gmail.com>
2019-03-22 09:55:11 +0100

    Codice Completato e Commentato

--- Rilevatore_Di_Riflessi/Rilevatore_Di_Riflessi.ino
+++ Rilevatore_Di_Riflessi/Rilevatore_Di_Riflessi.ino
@@ -1,51 +1,62 @@
 #include <LiquidCrystal_I2C.h>
-#define led 8
-#define pulsante1 4
-#define pulsante2 5
-#define pulsante3 6
-#define buzzer 7
-#define VERDE 10
-#define ROSSO 11
-#define BLU 12
-int counter = 0;
+
+//Dichiaro le variabili e le assegno
+#define button_start   4
+#define button_led     5
+#define button_buzzer  6
+#define buzzer         7
+#define led            8
+#define RED           10
+#define GREEN         11
+
+//Creo due variabili d'appoggio che assumeranno rispettivamente il valore in millisecondi del tempo di risposta di led e buzzer
+int result1;
+int result2;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup(){
   pinMode(led, OUTPUT);
-  pinMode(VERDE, OUTPUT);
-  pinMode(ROSSO, OUTPUT);
-  pinMode(BLU, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
   pinMode(buzzer, OUTPUT);
-  pinMode(pulsante1, INPUT);
-  pinMode(pulsante2, INPUT);
-  pinMode(pulsante3, INPUT);
+  pinMode(button_start, INPUT);
+  pinMode(button_led, INPUT);
+  pinMode(button_buzzer, INPUT);
   lcd.init();
   lcd.backlight();
 }
 
-void ciclo(int oggetto, int pulsante, int riga){
-  delay(random(2000, 15000));
-  digitalWrite(oggetto, HIGH);
-  if(oggetto == buzzer)
-    counter = 0;
-  while(digitalRead(pulsante)== LOW){
-    counter++;
+//Creo un unico ciclo int, così facendo posso estrapolare alla fine del primo ciclo il tempo di riflesso del led e assegnarlo a result1, nel secondo estrapolare il tempo di riflesso 
+//del buzzer e assegnarlo a result2
+int generic_loop(int object, int button, int row){
+  delay(random(2000, 10000));
+  digitalWrite(object, HIGH);
+  int result = 0;
+  while(digitalRead(button)== LOW){
+    result++;
     delay(1);
   }
-  digitalWrite(buzzer, LOW);
-  lcd.setCursor(0, riga);
-  lcd.print(counter);
+  digitalWrite(object, LOW);
+  lcd.setCursor(0, row);
+  lcd.print(result);
   lcd.print("ms");
-  if(counter <= 500)
-    digitalWrite(ROSSO, HIGH);
-  else
-    digitalWrite(VERDE, HIGH);
+  return result;
 }
 
+//Nel loop inserisco prima un if che fa partire i cicli solo se il button_start viene premuto, dopo i due cicli rispettivamente di led e buzzer inserisco un if che analizza i due
+//risultati e se entrambi sono inferiori a 500ms fa accendere il led RGB di verde, altrimenti di rosso
 void loop(){
-  if(digitalRead(pulsante1)== HIGH){
-     ciclo(led, pulsante2, 0);
-     ciclo(buzzer, pulsante3, 1);
+  if(digitalRead(button_start)== HIGH){
+    result1 = generic_loop(led, button_led, 0);
+    result2 = generic_loop(buzzer, button_buzzer, 1);
+    if(result1 <= 500 && result2 <= 500)
+      digitalWrite(GREEN, HIGH);
+    else
+      digitalWrite(RED, LOW);
+    delay(3500);
+    lcd.clear();
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, LOW);
   }
 }
